b1194c718d9edc886e07a667570c30d8287398b6
unknown <furkanyilmazx@gmail.com>
2019-01-26 00:13:56 +0300

    Add 404 Not found page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import HomePage from './HomePage';
 import ConnectedPage from './ConnectedPage';
+import NotFoundPage from './NotFoundPage';
 
-export { HomePage, ConnectedPage };
+export { HomePage, ConnectedPage, NotFoundPage };
