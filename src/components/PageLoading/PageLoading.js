f5aceaca86f452f480d953cc9b5b1fcf004c2576
Furkan YILMAZ <furkanyilmazx@gmail.com>
2019-03-23 22:00:40 +0300

    Fix lazy load pages with spinner (#8)

---
+++ src/components/PageLoading/PageLoading.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Spin, Icon, Row, Col } from 'antd';
+
+export default class PageLoading extends Component {
+  render() {
+    return (
+      <Row
+        type="flex"
+        justify="center"
+        align="middle"
+        style={{ height: '100%', width: '100%' }}>
+        <Col span={2}>
+          <Spin
+            size="large"
+            tip="Loading..."
+            indicator={<Icon type="loading" style={{ fontSize: 24 }} spin />}
+          />
+        </Col>
+      </Row>
+    );
+  }
+}
